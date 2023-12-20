f6df2de69b3c2976f55ec182b55c617fc003a498
Lena <girafffffmc@gmail.com>
2023-12-20 15:18:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pract26/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.pract26;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter = 1;
+    private static String CHANNEL_ID = "Напоминашка";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    CharSequence name = "Напоминание";
+                    String description = "Канал для напоминания долгов";
+                    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                    NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+                    channel.setDescription(description);
+                    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                    notificationManager.createNotificationChannel(channel);
+
+                }
+
+                Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
+               PendingIntent contentIntent = PendingIntent.getActivity(MainActivity.this,
+                        0, notificationIntent,
+                        PendingIntent.FLAG_IMMUTABLE);
+
+
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,
+                        0, notificationIntent,
+                        PendingIntent.FLAG_IMMUTABLE);
+
+                String bigText = "Открой сомкнуты негой взоры!" + " на встречу северной авроры!";
+
+                NotificationCompat.Builder builder =
+                        new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                                .setSmallIcon(R.drawable.bell)
+                                .setContentTitle("Пора красавица, проснись!!")
+                                .setContentText("Открой сомкнуты негой взоры!")
+                                .setStyle(new NotificationCompat.BigTextStyle().bigText(bigText))
+                                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                                .addAction(R.drawable.bell, "Открыть", pendingIntent)
+                                .addAction(R.drawable.bell, "Отказаться", pendingIntent)
+                                .setContentIntent(contentIntent)
+                                .setAutoCancel(true)
+                                // необязательные настройки
+                                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.push));
+
+                NotificationManagerCompat notificationManager =
+                        NotificationManagerCompat.from(MainActivity.this);
+                notificationManager.notify(counter++, builder.build());
+
+
+            }
+        });
+    }
+}
